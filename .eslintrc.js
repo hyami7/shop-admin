5c2795d12e4246400e75384e50c8836e88ec9cf4
yangwenjian <yangwenjian@sikulifang.com>
2023-05-25 15:12:27 +0800

    chore: 添加prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,39 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   globals: {
-    defineEmits: 'readonly',
-    defineProps: 'readonly',
-    defineExpose: 'readonly',
-    withDefaults: 'readonly'
+    defineEmits: "readonly",
+    defineProps: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
   },
-  extends: ['plugin:vue/vue3-strongly-recommended', 'standard'],
+  extends: [
+    "plugin:vue/vue3-strongly-recommended",
+    "standard",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: ['vue', '@typescript-eslint'],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
-    'vue/multi-word-component-names': 'off'
+    "vue/multi-word-component-names": "off",
+    semi: "off",
+    quotes: "off",
+    "space-before-function-paren": "off",
+    "comma-dangle": "off",
   },
   overrides: [
     {
-      files: ['src/api/**/*.ts'],
+      files: ["src/api/**/*.ts"],
       rules: {
-        camelcase: 'off'
-      }
-    }
-  ]
-}
+        camelcase: "off",
+      },
+    },
+  ],
+};
